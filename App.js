de069dcd76ba389b8dcd1307fbdb8bc9716a2f6c
RasitaT <rasitea@gmail.com>
2017-11-27 21:04:19 +0200

    funkcionalumas

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, AppRegistry,
+import { Platform, StyleSheet, Text, View, Alert, AppRegistry, TouchableOpacity, TouchableHighlight
        } from 'react-native';
 
 const instructions = Platform.select({
 
   
 });
+
+var backgroundcolors1 = ['blue', 'orange', 'purple', 'yellow'];
+var backgroundcolors2 = ['blue', 'orange', 'purple', 'yellow'];
+var backgroundcolors3 = ['blue', 'orange', 'purple', 'yellow'];
+var backgroundcolors4 = ['blue', 'orange', 'purple', 'yellow'];
 
 
 
@@ -27,38 +32,63 @@ export default class antrasdarbas extends Component<{ }> {
 }
 }
 
+_changeStyle1() {
+
+    var backgroundColor1 = backgroundcolors1[Math.floor(Math.random()*4)];
+    this.setState({
+        backgroundColor1: backgroundColor1
+    })
+}
 
 
 
-
+_changeStyle2() {
+    var backgroundColor2 = backgroundcolors2[Math.floor(Math.random()*4)];
+    this.setState({
+        backgroundColor2: backgroundColor2
+    })
+}
+_changeStyle3() {
+    var backgroundColor3 = backgroundcolors3[Math.floor(Math.random()*4)];
+    this.setState({
+        backgroundColor3: backgroundColor3
+    })
+}
+_changeStyle4() {
+    var backgroundColor4 = backgroundcolors4[Math.floor(Math.random()*4)];
+    this.setState({
+        backgroundColor4: backgroundColor4
+    })
+}
 
 
 render() {
     return (
         <View style={styles.container}>
             <View style={styles.flex1}>
-                
+                <TouchableOpacity onPress={ () => this._changeStyle1() }>
                     <Text style = {styles.button}>
                         Button1
                     </Text>
-              
-              
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={ () => this._changeStyle2() }>
                     <Text style = {styles.button}>
                         Button2
                     </Text>
-              
+                </TouchableOpacity>
 
-                
+                <TouchableOpacity onPress={ () => this._changeStyle3() }>
                     <Text style = {styles.button}>
                         Button3
                     </Text>
-              
+                </TouchableOpacity>
 
-              
+                <TouchableOpacity onPress={ () => this._changeStyle4() }>
                     <Text style = {styles.button}>
                         Button4
                     </Text>
-               
+                </TouchableOpacity>
 
             </View>
 
